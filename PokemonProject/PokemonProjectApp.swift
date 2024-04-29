a3cd073d7c128e692662315a6e956282ed781959
Ammad Ahmed <iosammadahmed@gmail.com>
2024-04-29 19:53:02 -0400

    Initial Commit

---
+++ PokemonProject/PokemonProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonProjectApp.swift
+//  PokemonProject
+//
+//  Created by A.d. Ahmed on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
